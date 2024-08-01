6d7370aa808ce25334848ae0cd3e4207cdc5ec3a
stijn <stijn.willems@mediahuis.com>
2024-08-01 13:21:09 +0200

    url swift-syntax to swiftlang
    this avoid warnings on resolution for others

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Depend on the latest Swift 5.10 release of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", "509.0.0"..<"601.0.0-prerelease"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "510.0.0"..<"601.0.0-prerelease"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
